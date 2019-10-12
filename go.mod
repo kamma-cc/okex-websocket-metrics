62ffeb5d4118b41b2083428d049868a654d2ad10
guangyu.ding <boaiwinserkua@gmail.com>
2019-10-12 23:42:42 +0800

    websocket

--- go.mod
+++ go.mod
@@ -2,4 +2,7 @@ module kamma.cc/okex-websocket-metrics
 
 go 1.13
 
-require github.com/prometheus/client_golang v1.1.0
+require (
+	github.com/gorilla/websocket v1.4.1
+	github.com/prometheus/client_golang v1.1.0
+)
--- main.go
+++ main.go
@@ -1,15 +1,48 @@
 package main
 
 import (
+	"github.com/gorilla/websocket"
 	"github.com/prometheus/client_golang/prometheus/promhttp"
 	"log"
 	"net/http"
+	"net/url"
 )
 
 func main() {
+	go runProm()
+	go connectOkex()
+	select {}
+}
+
+func runProm() {
 	http.Handle("/metrics", promhttp.Handler())
 	err := http.ListenAndServe(":2112", nil)
 	if err != nil {
 		log.Fatal(err)
 	}
+}
+func connectOkex() {
+
+	u := url.URL{
+		Scheme: "wss",
+		Host:   "okexcomreal.bafang.com:8443",
+		Path:   "ws/v3",
+	}
+	log.Printf("connecting to %s", u.String())
+	c, _, err := websocket.DefaultDialer.Dial(u.String(), nil)
+	if err != nil {
+		log.Fatal("dial:", err)
+	}
+	defer c.Close()
+	log.Printf("connected: %s", u.String())
+
+	for {
+		_, message, err := c.ReadMessage()
+		if err != nil {
+			log.Println("read:", err)
+			return
+		}
+		log.Printf("recv: %s", message)
+	}
+
 }
