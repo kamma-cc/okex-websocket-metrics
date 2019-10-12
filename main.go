4a649d193799c0c1a9facf2f483954d32a53c443
Guangyu Ding <guangyu.ding@okcoin.com>
2019-10-12 18:29:03 +0800

    error handler

--- main.go
+++ main.go
@@ -2,10 +2,14 @@ package main
 
 import (
 	"github.com/prometheus/client_golang/prometheus/promhttp"
+	"log"
 	"net/http"
 )
 
 func main() {
 	http.Handle("/metrics", promhttp.Handler())
-	http.ListenAndServe(":2112", nil)
+	err := http.ListenAndServe(":2112", nil)
+	if err != nil {
+		log.Fatal(err)
+	}
 }
